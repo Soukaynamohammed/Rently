002244f92f7c435c999d2f1dd0d4e8846bda3854
bridtsm <s154099@ap.be>
2024-11-19 22:12:15 +0100

    added bottom navigation

--- app/src/main/java/be/ap/student/mobiledev_rently/MenuActivity.kt
+++ app/src/main/java/be/ap/student/mobiledev_rently/MenuActivity.kt
@@ -2,6 +2,7 @@ package be.ap.student.mobiledev_rently
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import be.ap.student.mobiledev_rently.dataClasses.User
 import be.ap.student.mobiledev_rently.databinding.MenuScreenBinding
 
@@ -14,6 +15,46 @@ class MenuActivity : AppCompatActivity() {
         binding = MenuScreenBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        loadFragment(HomeFragment())
         supportActionBar?.hide()
+        binding.bottomNavBar.selectedItemId = R.id.navigation_home
+        binding.bottomNavBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.navigation_search_item->{
+                    //TODO: replace fragment
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.navigation_message->{
+                    //TODO: replace fragment
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.navigation_home->{
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.navigation_my_items->{
+                    //TODO: replace fragment
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.navigation_profile->{
+                    //TODO: replace fragment
+                    loadFragment(HomeFragment())
+                    true
+                }
+
+                else -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+            }
+        }
+    }
+    private fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
     }
 }
