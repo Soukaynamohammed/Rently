3e965befa6043b9bcd63613ca20d0f1f96427eb7
bridtsm <s154099@ap.be>
2024-11-27 11:41:29 +0100

    added image to user and parsing

--- app/src/main/java/be/ap/student/mobiledev_rently/dataClasses/User.kt
+++ app/src/main/java/be/ap/student/mobiledev_rently/dataClasses/User.kt
@@ -2,59 +2,65 @@ package be.ap.student.mobiledev_rently.dataClasses
 
 import android.os.Parcel
 import android.os.Parcelable
+import com.google.firebase.firestore.GeoPoint
+import kotlinx.parcelize.Parceler
+import kotlinx.parcelize.Parcelize
 
-class User () : Parcelable {
+@Parcelize
+class User () : Parcelable{
     private var email: String? = null
     private var username: String? = null
     private var password: String? = null
-    private var location: Map<String,String>? = null
+    private var location: GeoPoint? = null
+    private var imageUrl: String? = null
 
-    constructor(parcel: Parcel) : this() {
-        email = parcel.readString()
-        username = parcel.readString()
-        password = parcel.readString()
-    }
-
-    constructor(email: String, username: String, password: String, location: Map<String,String>?) : this() {
+    constructor(email: String?, username: String?, password: String?, location: GeoPoint?, imageUrl: String?) : this() {
         this.email = email
         this.username = username
         this.password = password
         this.location = location
+        this.imageUrl = imageUrl
     }
     fun getEmail(): String? { return email}
     fun getUsername(): String? {return username}
     fun getPassword(): String? {return password}
-    fun getLocation(): Map<String, String>? {return location}
+    fun getLocation(): GeoPoint? {return location}
     fun setEmail(email: String){this.email = email}
     fun setUsername(username: String){this.username = username}
     fun setPassword(password: String){this.password = password}
-    fun setLocation(location: Map<String, String>?){this.location = location}
+    fun setLocation(location: GeoPoint?){this.location = location}
     fun toMap(): Map<String, Any?>{
         return mapOf(
             "email" to email,
             "username" to username,
             "password" to password,
-            "location" to location
+            "location" to location,
+            "imageUrl" to imageUrl
         )
     }
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(email)
-        parcel.writeString(username)
-        parcel.writeString(password)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<User> {
-        override fun createFromParcel(parcel: Parcel): User {
-            return User(parcel)
+    private companion object : Parceler<User> {
+        override fun create(parcel: Parcel): User {
+            val email = parcel.readString()
+            val username = parcel.readString()
+            val password = parcel.readString()
+            val location = GeoPoint(parcel.readDouble(), parcel.readDouble())
+            val imageUrl = parcel.readString()
+            return User(email, username, password, location, imageUrl)
         }
 
-        override fun newArray(size: Int): Array<User?> {
-            return arrayOfNulls(size)
+        override fun User.write(parcel: Parcel, flags: Int) {
+            parcel.writeString(email)
+            parcel.writeString(username)
+            parcel.writeString(password)
+            if(location != null){
+                parcel.writeDouble(location!!.latitude)
+                parcel.writeDouble(location!!.longitude)
+            } else {
+                parcel.writeDouble(0.0)
+                parcel.writeDouble(0.0)
+            }
+            parcel.writeString(imageUrl)
         }
     }
 }
