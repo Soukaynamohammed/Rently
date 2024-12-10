1e34703533db40c025e613fa2eec16baa414a0fc
Soukaynamohammed <soukaynamohammmed@gmail.com>
2024-12-10 01:34:51 +0100

    list of rentals

--- app/src/main/java/be/ap/student/mobiledev_rently/RentalsFragment.kt
+++ app/src/main/java/be/ap/student/mobiledev_rently/RentalsFragment.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import be.ap.student.mobiledev_rently.adapter.RentalsAdapter
 import be.ap.student.mobiledev_rently.dataClasses.User
 import be.ap.student.mobiledev_rently.databinding.FragmentProfileBinding
 import be.ap.student.mobiledev_rently.databinding.FragmentRentalsBinding
+import be.ap.student.mobiledev_rently.util.FireBaseCommunication
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class RentalsFragment : Fragment() {
     private lateinit var binding: FragmentRentalsBinding
     var user: User? = null
+    private val adapter = RentalsAdapter()
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,8 +37,29 @@ class RentalsFragment : Fragment() {
         val view = binding.root
         binding.root
 
+        binding.recyclerViewRentals.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewRentals.adapter = adapter
+
+        fetchBookings()
+
         return view
     }
+
+    private fun fetchBookings() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val bookings = FireBaseCommunication().getBookingsByOwner(user!!.getEmail().toString())
+            bookings.forEach { booking ->
+                val itemId = booking.getItem()
+                if (!itemId.isNullOrEmpty()) {
+                    val item = FireBaseCommunication().getItemById(itemId)
+                }
+            }
+            CoroutineScope(Dispatchers.Main).launch {
+                adapter.submitList(bookings)
+            }
+        }
+    }
+
 
     companion object {
         @JvmStatic
